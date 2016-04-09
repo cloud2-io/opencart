6d5cc8844a008b60951f645ec6e989840fa37f18
Jean-Michel Esnault <jesnault@zokets.com>
2016-04-09 10:59:27 -0700

    fix dirname and log install and start time

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 if [ ! -f /var/www/html/index.php ] ; then
+ echo "`date -u` installing opencart"
  mkdir -p /var/www/html
  cp -r /workdir/* /var/www/html
  chmod -R o+w /var/www/html
 fi
-cd /var/wwww/html
+echo "`date -u` starting apache"
+cd /var/www/html
 apache2-foreground
